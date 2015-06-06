c16611d0b207fac301c6569f114db7ebdcc9a3fe
Keris Ward <kerisward@Keriss-MacBook-Pro.local>
2015-06-06 14:41:08 -0500

    adding Scene 2, Scene 3, Scene 5, FinalScene

--- smashfast.py
+++ smashfast.py
@@ -68,20 +68,29 @@ class Scene_1(Scene):
 class Scene_2(Scene):
 
     def enter(self):
-        print "description of scene 2"
+        print "As she rolls on her segway to the Forest she meets a friend. A life size Gummy "
+        print "Bear peaks from around the tree and joins on her journey."  
+        print "They soon hear a rustling up ahead in the trees. The Gummy runs"
+        print "ahead thinking it's his friend. Furiosa rides to protect her"
+        print "friend and the two are surprised to see an Alien standing in"
+        print "front of them. The Alien jumps on the Gummy Bear and tries to eat it."
+        print "The alien gets stuck in the GOO of the Gummy Bear, giving Furiosa"
+        print "a chance to arm herself or run."
+        print "\n"
+        print "Choose: 'save' the Gummy Bear, 'shoot' the alien, 'drive' away on the segway" 
 
         action = raw_input("> ")
 
-        if action == "option_a":
-            print "outcome of option_a"
+        if action == "drive":
+            print "She drives away on the segway."
             return 'scene3'
 
-        elif action == "option_b":
-            print "outcome of option_b"
+        elif action == "shoot":
+            print "She uses the shotgun and gets sprayed by acid."
             return 'death'
 
-        elif action == "option_c":
-            print "outcome of option c"
+        elif action == "save":
+            print "She tries to save her friend and gets sliced in half by the Alien's tail."
             return 'death'
 
         else:
@@ -91,20 +100,31 @@ class Scene_2(Scene):
 class Scene_3(Scene):
 
     def enter(self):
-        print "description of scene 3"
+        print "She approaches a meadow at the edge of the forest.  She sees" 
+        print "Hillary Clinton and Michelle Obama running to the doorway of the"
+        print "White House in the middle of the meadow. The first ladies call" 
+        print "Furiosa for help just as a family of Raptors fly in, catch them,"
+        print "and rip them to pieces. As she approaches the White House she"
+        print "loses control when her segway is cut in half under her feet."
+        print "She falls to the side to look back and see a huge BuZZ SAW"
+        print "monster coming up from under the ground. She reaches into the"
+        print "backpack and finds a Nerf supersoaker filled with water."
+        print "\n"
+        print "Choose: 'supersoaker' to spray the monster, 'shoot' to use the shotgun,"
+        print "'run' to run away"
 
         action = raw_input("> ")
 
-        if action == "option_a":
-            print "outcome of option_a"
+        if action == "supersoaker":
+            print "She sprays the monster and it rusts and crumbles."
             return 'scene4'
 
-        elif action == "option_b":
-            print "outcome of option_b"
+        elif action == "shoot":
+            print "She ignores the nerf gun and tries to use the shotgun again."
             return 'death'
 
-        elif action == "option_c":
-            print "outcome of option c"
+        elif action == "run":
+            print "She runs away but the monster is too fast."
             return 'death'
 
         else:
@@ -134,10 +154,49 @@ class Scene_4(Scene):
            print "DOES NOT COMPUTE!"
            return 'scene4'
 
+class Scene_5(Scene):
+
+    def enter(self):
+        print "She breaks through the Cheesy Spider webs and sees an elevator"
+        print "at the end of the tunnel. She gets in and heads to the top of"
+        print "the mountain not knowing that the Alien is hiding on the"
+        print "elevator roof. Ripley and Furiosa come face to face at the top"
+        print "of the Rock Candy Mountain. They share a snack of"
+        print "Twizzlers and Pixie Sticks. Just then the Alien jumps down to"
+        print "attack but Ripley pulls out a sniper rifle to fight back. It"
+        print "only shoots pixie sticks and is useless. In desperation,"
+        print "Furiosa throws the iPhone at the Alien who catches it and is"
+        print "mesmerized by Candy Crush. The Alien eyes are so fixed on the"
+        print "screen it starts to suck him into the phone and it sucks him in"
+        print "to another dimension. With the Alien gone the two feel safe and"
+        print "we think the game is won. HOWEVER..."
+        print "\n"
+        print "Choose: 'sonicscrewdriver' to fix the iPhone, 'play' Candy Crush"  
+
+        action = raw_input("> ")
+
+        if action == "sonicscrewdriver":
+            print "Ripley pulls out a Sonic Screwdriver and points it at the"
+            print "iPhone to fix it. This action creates a patch in the cosmic"
+            print "rip caused by the Tardis and the Universe is repaired!"
+            return 'finalscene'
+
+        elif action == "play":
+            print "Furiosa picks up the iPhone that has just sucked in the"
+            print "Alien and presses “Invite” to Ripley so that they can play."
+            print "Both are sucked into the phone and die."
+            return 'death'
+
+        else:
+            print "DOES NOT COMPUTE!"
+            return 'scene5'
+
 class Final_Scene(Scene):
 
     def enter(self):
-        print "You won! Good job."
+        print "The Universe is repaired! You saved the world! YOU WON!"
+        f = open('image.txt', 'r')
+        print f.read()
         exit()
 
 class Map(object):
@@ -148,6 +207,7 @@ class Map(object):
         'scene2': Scene_2(),
         'scene3': Scene_3(),
         'scene4': Scene_4(),
+        'scene5': Scene_5(),
         'finalscene': Final_Scene(),
         'death': Death(),
     }
@@ -161,7 +221,6 @@ class Map(object):
 
     def opening_scene(self):
         return self.next_scene(self.start_scene)
-
 
 a_map = Map('openingscene')
 a_game = Engine(a_map)
